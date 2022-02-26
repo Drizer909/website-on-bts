45b0c7bbec725214f1b2ca28563a50f4b2ba9df4
Drizer <shivam96670@gmail.com>
2022-02-26 03:13:16 -0800

    first saving point

---
+++ project 2/java.js
@@ -0,0 +1,40 @@
+
+
+
+var audio=new Audio("filter.mp3")
+function play(){
+    
+    audio.play()
+}
+function pause(){
+    audio.pause()
+    
+}
+var v1= new Audio("boy.mp3")
+function v(){
+    
+    v1.play()
+}
+function vo(){
+
+    v1.pause()
+}
+var jk1=new Audio("jk.mp3")
+function jk(){
+    jk1.play()
+}
+function jko(){
+    jk1.pause()
+}
+var rm1= new Audio("rm.mp3")
+function rm(){
+    
+    rm1.play()
+}
+function rmo(){
+    rm1.pause()
+}
+
+
+
+
